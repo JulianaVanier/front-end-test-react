c9334726cc4ab8caf6dcdb4b7b807770a3a40808
juliana silva <jusilva@newtrax.com>
2024-01-26 16:19:00 -0500

    dev progress

---
+++ src/utils/AlertDialogTrucks.test.tsx
@@ -0,0 +1,2 @@
+import { describe, expect, it } from 'vitest';
+
--- src/views/components/HeaderTrucks.tsx
+++ src/views/components/HeaderTrucks.tsx
@@ -1,20 +1,18 @@
 import { Typography } from "@mui/material";
 
 export default function HeaderTrucks() {
-
-
-	return (
-		<>
-			<Typography
-				variant="h5"
-				color="text.primary"
-				sx={{
-					m: 4,
-					padding: "0.7rem",
-				}}
-			>
-				Front End Test - React
-			</Typography>
-		</>
-	);
+  return (
+    <>
+      <Typography
+        variant="h5"
+        color="text.primary"
+        sx={{
+          m: 4,
+          padding: "0.7rem",
+        }}
+      >
+        Front End Newtrax
+      </Typography>
+    </>
+  );
 }
